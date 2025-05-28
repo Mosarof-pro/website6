697c738046a3700ab0afc252ace7f2209b1fe7e9
Mosarof-pro <mmd58553@gmail.com>
2025-05-28 11:45:14 +0200

    home page some code added

---
+++ node.js
@@ -0,0 +1,25 @@
+console.log(21);
+console.log('my name is mosarof hossen');
+
+
+var bannaPrice=12;
+console.log(bannaPrice);
+
+var isHot= true;
+var isRice= false;
+console.log( isHot);
+console.log(isRice)
+
+var Name="my name is mosarof hossen";
+console.log(Name);
+let Price="ok done my work";
+console.log(Price);
+
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
